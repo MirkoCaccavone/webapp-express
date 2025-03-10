f36c6aa5b57bd1491d02dee1624775328e583bf4
Mirko <mirkocaccavone03@gmail.com>
2025-03-10 14:24:44 +0100

    [feat] add show function to retrieve movie details and reviews

--- controllers/controller_routes.js
+++ controllers/controller_routes.js
@@ -15,4 +15,33 @@ function index(req, res) {
     });
 }
 
-module.exports = { index }
+// funzione SHOW
+function show(req, res) {
+    // recuperiamo l'id o
+    const { id } = req.params;
+
+    // prepariamo le query
+    const detailMovie = `SELECT * FROM movies WHERE movies.id = ?`;
+
+    const reviewSql = `SELECT * FROM reviews WHERE movie_id = ?`;
+
+
+    // eseguiamo la query per mostrare un singolo film
+    connection.query(detailMovie, [id], (err, results) => {
+        if (err) return res.status(500).json({ error: 'Database query failed' })
+        if (results.length === 0) return res.status(404).json({ error: 'Movie not found' })
+
+        const movie = results[0];
+
+        connection.query(reviewSql, [id], (err, reviewResult) => {
+
+            if (err) return res.status(500).json({ error: 'Database query failed' });
+            // aggiorniamo con le review ritornate
+            movie.reviews = reviewResult;
+            // ritorniamo l'oggetto completo
+            res.json(movie);
+        });
+    })
+}
+
+module.exports = { index, show }
--- routers/routes.js
+++ routers/routes.js
@@ -7,6 +7,6 @@ const routesController = require('../controllers/controller_routes')
 router.get('/', routesController.index);
 
 // show visualizza un elemento per id
-router.get('/:id');
+router.get('/:id', routesController.show);
 
 module.exports = router;
